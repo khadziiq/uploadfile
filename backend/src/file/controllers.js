9dc6f84dd49c6bc66c62fe1b0aebedc90495b2f2
khoirulkahf <fahrulista@gmail.com>
2022-08-02 22:19:24 +0700

    new

--- backend/src/file/controllers.js
+++ backend/src/file/controllers.js
@@ -1,7 +1,7 @@
 const pool = require("../../db");
 const queries = require("./queries");
 const path = require("path");
-// const { Blob } = require("buffer");
+const { Blob } = require("buffer");
 
 const getFiles = (req, res) => {
   pool.query(queries.getFiles, (error, result) => {
@@ -14,14 +14,29 @@ const getFileById = (req, res) => {
   const id = parseInt(req.params.id);
   pool.query(queries.getFileById, [id], (error, result) => {
     if (error) throw error;
-    res.status(200).json(result.rows);
-  });
-};
+    if (result.rowCount > 0) {
+      const row = result.rows[0];
+      const extname = path.extname(row.filename);
+      let fileType = "image/jpeg";
+      switch (extname) {
+        case "png":
+          fileType = "image/png";
+        case "jpg":
+          fileType = "image/jpeg";
+        default:
+          fileType = "image/jpeg";
+          break;
+      }
 
-const uploadFile = (req, res) => {
-  let finalImageUrl =
-    req.protocol + "://" + req.get("host") + "/uploads/" + req.file.filename;
-  res.json({ status: "success", image: finalImageUrl });
+      const data = new Blob(row.filepars, { type: fileType });
+      data.filename = row.filename;
+      res.setHeader("Content-Length", row.filepars.length);
+      console.log(data);
+      res.write(data, "binary");
+      res.end();
+    }
+    return res.status(404);
+  });
 };
 
 const uploadFileByteArray = (req, res) => {
@@ -40,7 +55,6 @@ const uploadFileByteArray = (req, res) => {
 
 module.exports = {
   getFiles,
-  uploadFile,
   getFileById,
   uploadFileByteArray,
 };
--- backend/src/file/routes.js
+++ backend/src/file/routes.js
@@ -1,30 +1,10 @@
 const { Router } = require("express");
 const controller = require("./controllers");
 
-const path = require("path");
-const multer = require("multer");
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      path.parse(file.originalname).name +
-        "-" +
-        Date.now() +
-        path.extname(file.originalname)
-    );
-  },
-});
-const upload = multer({ storage });
-
 const router = Router();
 
 router.get("/", controller.getFiles);
-router.post("/uploadImage", controller.uploadFileByteArray);
-router.post("/upload", upload.single("photo"), controller.uploadFile);
+router.post("/upload", controller.uploadFileByteArray);
 router.get("/:id", controller.getFileById);
 
 module.exports = router;
